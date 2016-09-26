b51b99cca298a5b22fdd20f3622d922f8ab245ba
Harsh Yadav <harsh.yadav@magicsw.com>
2016-09-26 12:12:42 +0530

    first commit

---
+++ Client/app.js
@@ -0,0 +1,50 @@
+// (function(){
+
+  var app = angular.module('app', ['ui.router'])
+  app.config([
+    '$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+    .state('home', {
+      url: '/',
+       templateUrl: './templates/contentPanel.html',
+       controller:'ContentPanelController'   
+      // views: {
+      //   '': {
+      //     templateUrl: './templates/main.html'
+      //   },
+      //   'nav@home': {
+      //     templateUrl: './templates/assets/nav.html'
+      //   },
+      //   'body@home': {
+      //     templateUrl: './templates/body.html'
+      //   },
+      //   'footer@home': {
+      //     templateUrl: './templates/assets/footer.html'
+      //   }
+      // }
+    });
+    // .state('main', {
+    //   url: '/main',        
+     
+        
+    // });
+  }]);
+
+  app.controller('SidebarController', function($scope,$rootScope) {
+    
+    $rootScope.isShown = true;
+    
+    $scope.toggleState = function() {
+        $scope.state = !$scope.state;
+    };
+    
+});
+
+   app.controller('ContentPanelController', function($scope,$rootScope) {
+    
+    $rootScope.isShown = false;
+    
+  
+});
+// });
